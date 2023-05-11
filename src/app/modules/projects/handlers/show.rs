a4cc042537b799c39ab1eb678d2381553dc0390b
kennycallado <kennycallado@hotmail.com>
2023-05-11 16:12:31 +0200

    UPDATE Integara values en project (show)

--- src/app/modules/projects/handlers/show.rs
+++ src/app/modules/projects/handlers/show.rs
@@ -4,7 +4,10 @@ use rocket::serde::json::Json;
 use crate::app::providers::interfaces::helpers::claims::UserInClaims;
 use crate::config::database::Db;
 
-use crate::app::modules::projects::model::Project;
+use crate::app::modules::project_values::services::repository as pv_repository;
+use crate::app::modules::values::services::repository as values_repository;
+
+use crate::app::modules::projects::model::{Project, ProjectWithValues};
 use crate::app::modules::projects::services::repository as projects_repository;
 
 pub async fn get_show_admin(db: &Db, _admin: UserInClaims, id: i32) -> Result<Json<Project>, Status> {
@@ -14,4 +17,61 @@ pub async fn get_show_admin(db: &Db, _admin: UserInClaims, id: i32) -> Result<Js
         Ok(project) => Ok(Json(project)),
         Err(_) => Err(Status::InternalServerError),
     }
+}
+
+pub async fn get_show_values_admin(db: &Db, _admin: UserInClaims, id: i32) -> Result<Json<ProjectWithValues>, Status> {
+    let project = projects_repository::get_by_id(&db, id).await;
+    if let Err(_) = project {
+        return Err(Status::InternalServerError);
+    }
+    let project = project.unwrap();
+    
+    let project_values = pv_repository::get_by_project_id(&db, id).await;
+    if let Err(_) = project_values {
+        return Err(Status::InternalServerError);
+    }
+    let project_values = project_values.unwrap();
+
+    let mut values = vec![];
+    for pv in project_values {
+        let value = values_repository::get_by_id(&db, pv.values_id).await;
+        if let Err(_) = value {
+            return Err(Status::InternalServerError);
+        }
+        let value = value.unwrap();
+
+        values.push(value);
+    }
+
+    let project = ProjectWithValues {
+        id: project.id,
+        name: project.name,
+        keys: project.keys,
+        values: Some(values),
+    };
+
+    Ok(Json(project))
+}
+
+pub async fn get_show_user_values_admin(db: &Db, _admin: UserInClaims, id: i32, user_id: i32) -> Result<Json<ProjectWithValues>, Status> {
+    let project = projects_repository::get_by_id(&db, id).await;
+    if let Err(_) = project {
+        return Err(Status::InternalServerError);
+    }
+    let project = project.unwrap();
+    
+    let values = values_repository::get_all_by_user_id(&db, user_id).await;
+    if let Err(_) = values {
+        return Err(Status::InternalServerError);
+    }
+    let values = values.unwrap();
+
+    let project = ProjectWithValues {
+        id: project.id,
+        name: project.name,
+        keys: project.keys,
+        values: Some(values),
+    };
+
+    Ok(Json(project))
 }
