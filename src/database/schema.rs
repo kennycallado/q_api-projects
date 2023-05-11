020755c9b2d416e48f177eee96ba948b993c5383
kennycallado <kennycallado@hotmail.com>
2023-05-11 16:10:45 +0200

    ADD Agrega migraciones project_values y values

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -1,9 +1,34 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    project_values (id) {
+        id -> Int4,
+        project_id -> Int4,
+        values_id -> Int4,
+    }
+}
+
+diesel::table! {
     projects (id) {
         id -> Int4,
         name -> Varchar,
         keys -> Array<Nullable<Text>>,
     }
 }
+
+diesel::table! {
+    values (id) {
+        id -> Int4,
+        user_id -> Int4,
+        value -> Jsonb,
+    }
+}
+
+diesel::joinable!(project_values -> projects (project_id));
+diesel::joinable!(project_values -> values (values_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    project_values,
+    projects,
+    values,
+);
